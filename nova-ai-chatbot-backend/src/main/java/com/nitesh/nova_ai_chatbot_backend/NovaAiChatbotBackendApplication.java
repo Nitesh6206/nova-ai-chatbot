6fff93d89ef09bd92ec63d2426dd46a1632a5a4d
Nitesh6206 <niteshsingh6206@gmail.com>
2026-05-23 11:43:13 +0530

    initial commit

---
+++ nova-ai-chatbot-backend/src/main/java/com/nitesh/nova_ai_chatbot_backend/NovaAiChatbotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nitesh.nova_ai_chatbot_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaAiChatbotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovaAiChatbotBackendApplication.class, args);
+	}
+
+}
---
+++ nova-ai-chatbot-backend/src/test/java/com/nitesh/nova_ai_chatbot_backend/NovaAiChatbotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nitesh.nova_ai_chatbot_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovaAiChatbotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
